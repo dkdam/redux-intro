6e45476424eccc0517ed625f568a4800a37f8d08
dkdam <dkdam@live.com>
2022-11-28 18:39:57 +1100

    modified some actions and now it counts

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import allReducers from './reducers'
+import { Provider } from 'react-redux'
 
-const store = createStore(
+const store = createStore( 
   allReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function

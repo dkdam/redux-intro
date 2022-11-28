bef6396fc35cb8785cf6a1abc31883559b54b65c
dkdam <dkdam@live.com>
2022-11-28 17:45:57 +1100

    learning redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+
+//STORE => GLOBALIZED STATE
+
+//ACTION INCREMENT
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// REDUCER
+const counter = ( state = 0, action ) => {
+  // eslint-disable-next-line default-case
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+let store = createStore(counter)
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()))
+
+//DISPATCH
+store.dispatch(increment())
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
